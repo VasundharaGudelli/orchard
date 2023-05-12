fd3cb360a7a3eec467255bc66821caa9c831a4db
Alex Hester <amhester@anderson.edu>
2023-05-12 09:33:16 -0400

    Fri 2889 (#33)
    * feat: add paging to sync crm roles
    
    * chore: upgrade protos

--- cmd/server/grpc/grpc.go
+++ cmd/server/grpc/grpc.go
@@ -4,12 +4,14 @@ import (
 	"context"
 
 	"github.com/loupe-co/bouncer/pkg/client"
+	"github.com/loupe-co/go-common/errors"
 	"github.com/loupe-co/orchard/internal/clients"
 	"github.com/loupe-co/orchard/internal/config"
 	"github.com/loupe-co/orchard/internal/db"
 	"github.com/loupe-co/orchard/internal/handlers"
 	orchardPb "github.com/loupe-co/protos/src/common/orchard"
 	servicePb "github.com/loupe-co/protos/src/services/orchard"
+	"google.golang.org/grpc/codes"
 )
 
 type OrchardGRPCServer struct {
@@ -245,4 +247,8 @@ func (server *OrchardGRPCServer) GetTenantPersonCount(ctx context.Context, in *s
 
 func (server *OrchardGRPCServer) GetPeopleByEmail(ctx context.Context, in *servicePb.GetPeopleByEmailRequest) (*servicePb.GetPeopleByEmailResponse, error) {
 	return server.handlers.GetPeopleByEmail(ctx, in)
+}
+
+func (server *OrchardGRPCServer) GetOutreachUserCommitMappings(ctx context.Context, in *servicePb.GetOutreachUserCommitMappingsRequest) (*servicePb.GetOutreachUserCommitMappingsResponse, error) {
+	return nil, errors.New("unimplemented").WithCode(codes.Unimplemented).Clean().AsGRPC()
 }
